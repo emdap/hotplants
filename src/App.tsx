f925ef1b504f3250ee4f1adf2eba62d8b9f1878b
Emma DaPonte <emma@mymarky.ai>
2025-07-08 17:55:25 -0700

    test out getting gbif data -- native plants in polygon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import createClient from "openapi-fetch";
 import DarkModeToggle from "./components/DarkModeToggle";
+import type { paths } from "./schemas/gbif";
 
-function App() {
+const App = () => {
+  const client = createClient<paths>({ baseUrl: "https://api.gbif.org/v1/" });
+  client.GET("/occurrence/search", {
+    params: {
+      query: {
+        geometry: [
+          "POLYGON((-124.48 32.53,-124.48 42.01,-114.13 42.01,-114.13 32.53,-124.48 32.53))",
+        ],
+        establishmentMeans: ["native"],
+        limit: 10,
+      },
+    },
+  });
+
   return (
     <>
       <DarkModeToggle />
       <div className=" text-black">hey</div>
     </>
   );
-}
+};
 
 export default App;
