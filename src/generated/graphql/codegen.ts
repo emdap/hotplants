2a49122afd01bb2b90d46e9c15f253a7846f9c03
Emma DaPonte <emma@mymarky.ai>
2025-10-10 09:13:46 -0700

    update config and turn off fragment masking

--- src/generated/graphql/codegen.ts
+++ src/generated/graphql/codegen.ts
@@ -2,11 +2,20 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "http://localhost:3000/graphql",
-  documents: ["src/**/*.tsx"],
+  documents: ["src/**/*.tsx", "src/**/*.ts"],
   ignoreNoDocuments: true,
+  config: {
+    maybeValue: "T | null",
+    skipTypename: true,
+    nullability: false,
+  },
+
   generates: {
     "src/generated/graphql/": {
       preset: "client",
+      presetConfig: {
+        fragmentMasking: false,
+      },
     },
     "src/generated/graphql/schema.graphql": {
       plugins: ["schema-ast"],
