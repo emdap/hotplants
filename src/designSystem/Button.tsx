20d2def50cf0f6a158639dd08e3c02f7369aed72
Emma DaPonte <emma@mymarky.ai>
2025-10-13 18:12:12 -0700

    more button options

--- src/designSystem/Button.tsx
+++ src/designSystem/Button.tsx
@@ -2,20 +2,25 @@ import classNames from "classnames";
 import { ButtonHTMLAttributes } from "react";
 
 const Button = ({
-  variant,
+  variant = "default",
   className,
   ...buttonProps
 }: {
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "text" | "default";
 } & ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
     className={classNames(
-      "cursor-pointer rounded-sm max-w-fit hover:shadow-sm",
+      "rounded-sm max-w-fit flex items-center justify-center",
       className,
       {
         "bg-primary/80 hover:bg-primary": variant === "primary",
         "bg-secondary/80 hover:bg-secondary": variant === "secondary",
-        "hover:bg-secondary/10": !variant,
+        "hover:bg-secondary/10": variant === "default",
+        "hover:shadow-sm": variant !== "text",
+
+        "cursor-pointer": !buttonProps.disabled,
+        "opacity-50 hover:bg-transparent! hover:shadow-none!":
+          buttonProps.disabled,
       }
     )}
     {...buttonProps}
