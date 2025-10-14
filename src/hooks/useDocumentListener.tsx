5b0d8e59e86c9ee1b7be006a1d54263f91ea9b1f
Emma DaPonte <emma@mymarky.ai>
2025-10-13 18:50:50 -0700

    add capture option

--- src/hooks/useDocumentListener.tsx
+++ src/hooks/useDocumentListener.tsx
@@ -3,12 +3,13 @@ import { useEffect } from "react";
 export const useDocumentListener = <T extends keyof DocumentEventMap>(
   eventKey: T,
   onEventCallback: (e: DocumentEventMap[T]) => void,
-  enabled = true
+  enabled = true,
+  onCapture?: boolean
 ) => {
   useEffect(() => {
     if (enabled) {
-      document.addEventListener(eventKey, onEventCallback);
+      document.addEventListener(eventKey, onEventCallback, onCapture);
     }
     return () => document.removeEventListener(eventKey, onEventCallback);
-  }, [onEventCallback, eventKey, enabled]);
+  }, [onEventCallback, eventKey, enabled, onCapture]);
 };
