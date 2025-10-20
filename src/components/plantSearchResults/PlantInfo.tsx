a59ee99adc4762180e62525ec27fb604ed5af56b
Emma DaPonte <emma@mymarky.ai>
2025-10-20 13:46:28 -0700

    allow selecting text on plant card without clicking into plant

--- src/components/plantSearchResults/PlantInfo.tsx
+++ src/components/plantSearchResults/PlantInfo.tsx
@@ -2,7 +2,9 @@ import Card from "designSystem/Card";
 import { PlantResult } from "graphqlHelpers/plantQueries";
 
 const PlantInfo = ({ plant }: { plant: PlantResult }) => {
-  return <Card> {plant.scientificName} </Card>;
+  return (
+    <Card onClick={(e) => e.stopPropagation()}>{plant.scientificName}</Card>
+  );
 };
 
 export default PlantInfo;
