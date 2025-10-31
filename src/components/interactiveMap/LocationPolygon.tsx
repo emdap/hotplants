771051ef379dfd1a9566842e5f618c98ca420439
Emma DaPonte <emma@mymarky.ai>
2025-10-30 17:59:17 -0700

    move meridian crossing warning to separate component, need to use raw leaflet to add/remove the tooltip to prevent TS errors; center the map on mount

--- src/components/interactiveMap/LocationPolygon.tsx
+++ src/components/interactiveMap/LocationPolygon.tsx
@@ -1,9 +1,11 @@
 import { usePlantSearchContext } from "contexts/PlantSearchContext";
-import { LatLngTuple, LeafletEvent, Marker as MarkerType } from "leaflet";
+import { LeafletEvent, Marker as MarkerType } from "leaflet";
 import { useEffect, useMemo } from "react";
-import { Marker, Polygon, Tooltip, useMap } from "react-leaflet";
+import { Marker, Polygon, useMap } from "react-leaflet";
+import { useMount } from "react-use";
 import { LocationCoord } from "schemaHelpers/customSchemaTypes";
 import { LocationWithPolygon } from "schemaHelpers/schemaTypesUtil";
+import CrossingMerdianTooltip from "./CrossingMerdianTooltip";
 
 // GeoJSON spec dictates lng, lat
 // Leaflet uses lat, lng
@@ -13,41 +15,24 @@ const swapLatLng = (coords: number[][] | LocationCoord[]): LocationCoord[] =>
 const LocationPolygon = ({
   boundingPolygon,
   locationSource,
-  centerPoint: {
-    geometry: { coordinates: centerCoordinates },
-  },
+  centerPoint,
 }: LocationWithPolygon) => {
   const map = useMap();
+  useMount(() => map.fitBounds(polyCoords));
+
   const { setCustomLocationPolygon } = usePlantSearchContext();
-
-  const centerPoint: LatLngTuple = useMemo(
-    () => [centerCoordinates[1], centerCoordinates[0]],
-    [centerCoordinates]
-  );
-
-  const showMeridianWarning = useMemo(() => {
-    const { coordinates } = boundingPolygon.geometry;
-    const hasCoords: Record<string, boolean> = { "180": false, "-180": false };
-
-    coordinates[0].forEach(([lng]) => {
-      if ([180, -180].includes(lng)) {
-        hasCoords[String(lng)] = true;
-      }
-    });
-
-    return hasCoords["180"] && hasCoords["-180"];
-  }, [boundingPolygon.geometry]);
-
-  useEffect(() => {
-    locationSource === "search" &&
-      map.setView(centerPoint, showMeridianWarning ? 1 : 10);
-  }, [centerPoint, locationSource, map, showMeridianWarning]);
 
   // Only plotting outer ring of geometry
   const polyCoords = useMemo(
     () => swapLatLng(boundingPolygon.geometry.coordinates[0]),
     [boundingPolygon.geometry.coordinates]
   );
+
+  useEffect(() => {
+    if (locationSource === "search") {
+      map.fitBounds(polyCoords);
+    }
+  }, [polyCoords, locationSource, map]);
 
   const markerDragEnd = (e: LeafletEvent, index: number) => {
     const corner = e.target as MarkerType;
@@ -64,15 +49,8 @@ const LocationPolygon = ({
 
   return (
     <>
-      <Polygon positions={polyCoords}>
-        {showMeridianWarning && (
-          <Tooltip position={centerPoint} direction="top">
-            Bounding boxes crossing the international date line will not display
-            correctly
-          </Tooltip>
-        )}
-      </Polygon>
-
+      <Polygon positions={polyCoords} />
+      <CrossingMerdianTooltip {...{ centerPoint, boundingPolygon }} />
       {polyCoords.slice(0, -1).map((point, index) => (
         // Omit connecting point at end
         <Marker
