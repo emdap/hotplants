25936af18dc274cddac94498d44f5e0da45ed883
Emma DaPonte <ecodapo@gmail.com>
2026-02-04 15:38:36 -0800

    re-enable html scroll when component dismounts

--- client/src/hooks/useDisableHtmlScroll.tsx
+++ client/src/hooks/useDisableHtmlScroll.tsx
@@ -4,12 +4,18 @@ export const useDisableHtmlScroll = (hideScroll?: boolean) => {
   const toggleHideScroll = (hide?: boolean) =>
     document.documentElement.classList.toggle(
       "small-screen:overflow-hidden!",
-      Boolean(hide)
+      Boolean(hide),
     );
 
   useEffect(() => {
     toggleHideScroll(hideScroll);
   }, [hideScroll]);
+
+  useEffect(() => {
+    return () => {
+      toggleHideScroll(false);
+    };
+  }, []);
 
   return {
     toggleHideScroll,
