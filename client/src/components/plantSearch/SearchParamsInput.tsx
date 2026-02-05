e81e0eb6af42c950510c066bcb4445f3e4637966
Emma DaPonte <ecodapo@gmail.com>
2026-02-05 12:29:53 -0800

    separate out location controls from overall search params component

--- client/src/components/plantSearch/SearchParamsInput.tsx
+++ client/src/components/plantSearch/SearchParamsInput.tsx
@@ -1,17 +1,8 @@
-import classNames from "classnames";
-import MapProvider from "components/interactiveMap/MapProvider";
 import { usePlantSearchContext } from "contexts/plantSearch/PlantSearchContext";
 import Button from "designSystem/Button";
-import Card from "designSystem/Card";
-import { useReactQuery } from "hooks/useQuery";
 import _ from "lodash";
-import { FormEvent, useEffect, useState } from "react";
-import { useDebounce } from "react-use";
-import {
-  customLocationDisplay,
-  lookupLocationInput,
-  validateNominatimLocation,
-} from "util/locationUtil";
+import { FormEvent, useState } from "react";
+import LocationParamsInput from "./LocationParamsInput";
 import OptionalSearchParamsInput from "./OptionalSearchParamsInput";
 
 const SearchParamsInput = () => {
@@ -20,71 +11,15 @@ const SearchParamsInput = () => {
     hasCurrentResults,
     searchParams,
     searchParamsDraft,
-    validatedSearchParamsDraft,
-    updateSearchParamsDraft,
     applySearchParams,
   } = usePlantSearchContext();
 
-  const [searchInput, setSearchInput] = useState("");
-  const [debouncedInput, setDebouncedInput] = useState<string | null>(null);
-  const [locationInvalid, setLocationInvalid] = useState(false);
-
-  useDebounce(
-    () =>
-      (debouncedInput !== null || searchInput) &&
-      setDebouncedInput(searchInput),
-    1000,
-    [searchInput],
-  );
-
-  useEffect(() => {
-    if (searchParamsDraft?.locationSource === "custom") {
-      setLocationInvalid(false);
-    }
-  }, [searchParamsDraft?.locationSource]);
-
-  useEffect(() => {
-    setSearchInput(searchParams?.locationName ?? "");
-    setDebouncedInput(searchParams?.locationName ?? "");
-  }, [searchParams?.locationName]);
-
-  const locationQuery = useReactQuery({
-    queryKey: ["location-search", debouncedInput],
-    enabled:
-      debouncedInput !== null &&
-      searchParams?.locationName !== null &&
-      searchParams?.locationName !== debouncedInput,
-    retry: false,
-    queryFn: async () => {
-      setLocationInvalid(false);
-
-      if (debouncedInput) {
-        const { data } = await lookupLocationInput(debouncedInput);
-        const validLocation = validateNominatimLocation(data?.[0]);
-        if (validLocation) {
-          updateSearchParamsDraft(validLocation);
-          return validLocation;
-        } else {
-          setLocationInvalid(true);
-        }
-      }
-
-      return null;
-    },
-  });
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== "Enter" || !(e.target instanceof HTMLInputElement)) {
-      return;
-    }
-    setDebouncedInput(e.target.value);
-  };
+  const [locationPending, setLocationPending] = useState(false);
 
   const disableSubmit =
+    locationPending ||
     !searchParamsDraft ||
-    _.isEqual(searchParamsDraft, searchParams) ||
-    searchInput !== debouncedInput ||
-    locationQuery.isLoading;
+    _.isEqual(searchParamsDraft, searchParams);
 
   const submitLocation = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -98,45 +33,7 @@ const SearchParamsInput = () => {
       onSubmit={submitLocation}
       className="h-full flex flex-col gap-4 overflow-auto pb-4 [&_.card]:pt-4"
     >
-      <Card className="flex flex-col gap-2">
-        <div className="form-item">
-          <label htmlFor="search-location" className="max-w-fit pb-2">
-            <h2>Location</h2>
-          </label>
-          <input
-            id="search-location"
-            value={searchInput}
-            className={classNames(
-              "styled-input flex-grow min-w-20",
-              (locationQuery.isError || locationInvalid) &&
-                "dark:!border-red-700 ring-offset-red-500/70 !border-red-500",
-            )}
-            onBlur={() => setDebouncedInput(searchInput)}
-            onKeyDown={handleKeyDown}
-            onChange={(e) => setSearchInput(e.target.value)}
-            placeholder={
-              searchParamsDraft?.locationSource === "custom"
-                ? customLocationDisplay(searchParamsDraft)
-                : "Enter Location"
-            }
-          />
-        </div>
-
-        <div className="px-1 text-xs font-medium text-default-text/70">
-          {locationQuery.isError
-            ? "Error loading location"
-            : locationInvalid && "Cannot find location"}
-        </div>
-
-        <MapProvider
-          locationCustomizeable
-          isLoading={locationQuery.isLoading || locationQuery.isFetching}
-          searchParams={validatedSearchParamsDraft}
-          setSearchParams={updateSearchParamsDraft}
-          className="w-full h-[200px] lg:h-[300px] grow"
-        />
-      </Card>
-
+      <LocationParamsInput setLocationPending={setLocationPending} />
       <OptionalSearchParamsInput />
 
       {(hasCurrentResults || searchParamsDraft) && (
