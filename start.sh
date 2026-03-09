baefec899dc90cd6983f98308d1aff76c18a25fe
Emma DaPonte <ecodapo@gmail.com>
2026-03-09 15:07:01 -0700

    update start script to ping BE in background; add deploy script to server

--- start.sh
+++ start.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
+# Wake-up backend in background to minimize user delays
+wget -q -O /dev/null https://hotplants-server.fly.dev/health
+
 # Start the Node server in the background
 node server/dist/index.js &
 
 
-# Wait for Node server to be available before exposing frontend
+# Wait for Node proxy server to be available before exposing frontend
 MAX_TRIES=20
 COUNT=0
 
 until nc -z 0.0.0.0 4000; do
   COUNT=$((COUNT + 1))
   if [ $COUNT -ge $MAX_TRIES ]; then
-    echo "Server did not start after $MAX_TRIES attempts, exiting"
+    echo "Proxy server did not start after $MAX_TRIES attempts, exiting"
     exit 1
   fi
-  echo "Waiting for server... attempt $COUNT/$MAX_TRIES"
+  echo "Waiting for proxy server... attempt $COUNT/$MAX_TRIES"
   sleep 0.5
 done
-
-echo "Server is up"
-
 
 # Start nginx in the foreground
 nginx -g 'daemon off;'
