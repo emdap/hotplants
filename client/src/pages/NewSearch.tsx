76018e9301b8ca4533011e79276e17494426cd0a
Emma DaPonte <ecodapo@gmail.com>
2026-04-14 11:33:09 -0700

    fix position of plant animation when sidebar expanded

--- client/src/pages/NewSearch.tsx
+++ client/src/pages/NewSearch.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "@tanstack/react-router";
 import PlantAnimation from "components/PlantAnimation";
 import PlantLocationForm from "components/plantDataControls/plantLocation/PlantLocationForm";
 import PlantNameForm from "components/plantDataControls/plantName/PlantNameForm";
+import { useAppContext } from "contexts/AppContext";
 import { useSearchParamsContext } from "contexts/searchParams/SearchParamsContext";
 import Button from "designSystem/Button";
 import PageTitle from "designSystem/PageTitle";
@@ -14,14 +15,23 @@ import {
   useState,
 } from "react";
 
+const DEFAULT_ANIMATION_CONTAINER_STYLE: HTMLAttributes<HTMLDivElement>["style"] =
+  { position: "static", translate: "unset" };
+const ANIMATION_GAP = 30;
+const SIDEBAR_WIDTH = 260;
+
 const NewSearch = () => {
   const navigate = useNavigate();
   const animation = useAnimation();
+
+  const { sidebarExpanded } = useAppContext();
   const { searchParamsDraft, applySearchParams } = useSearchParamsContext();
 
-  const animationContainerRef = useRef<HTMLDivElement>(null);
-  const [animationContainerStyle, setAnimationContainerStyle] =
-    useState<HTMLAttributes<HTMLDivElement>["style"]>();
+  const paramsContainerRef = useRef<HTMLDivElement>(null);
+  const animationRef = useRef<HTMLDivElement>(null);
+  const [animationContainerStyle, setAnimationContainerStyle] = useState(
+    DEFAULT_ANIMATION_CONTAINER_STYLE,
+  );
 
   const paramValues = JSON.stringify(searchParamsDraft);
 
@@ -34,14 +44,49 @@ const NewSearch = () => {
   }, [animation, paramValues]);
 
   useLayoutEffect(() => {
+    setAnimationContainerStyle((prev) => {
+      if (animationRef.current && prev.left && Number(prev.left)) {
+        const leftOffset =
+          Number(prev.left) + (sidebarExpanded ? 1 : -1) * SIDEBAR_WIDTH;
+        if (
+          leftOffset + animationRef.current.clientWidth <
+          window.innerWidth - ANIMATION_GAP
+        ) {
+          return {
+            ...prev,
+            left:
+              Number(prev.left) + (sidebarExpanded ? 1 : -1) * SIDEBAR_WIDTH,
+          };
+        }
+      }
+
+      return DEFAULT_ANIMATION_CONTAINER_STYLE;
+    });
+  }, [sidebarExpanded]);
+
+  useLayoutEffect(() => {
     const updateContainerStyle = () => {
-      if (!animationContainerRef.current) {
-        setAnimationContainerStyle({ position: "static" });
+      if (!animationRef.current || !paramsContainerRef.current) {
+        setAnimationContainerStyle(DEFAULT_ANIMATION_CONTAINER_STYLE);
       } else {
-        const availSpace = Math.min(window.innerWidth / 2, 600);
-        setAnimationContainerStyle({
-          left: animationContainerRef.current.clientWidth / 2 + availSpace,
-        });
+        const leftOffset =
+          paramsContainerRef.current.offsetLeft +
+          paramsContainerRef.current.clientWidth +
+          ANIMATION_GAP;
+
+        const availSpace = window.innerWidth - leftOffset;
+        const animationWidth = animationRef.current.clientWidth;
+
+        if (availSpace - ANIMATION_GAP < animationWidth) {
+          setAnimationContainerStyle(DEFAULT_ANIMATION_CONTAINER_STYLE);
+        } else {
+          setAnimationContainerStyle({
+            right: 10,
+            left:
+              leftOffset + (Math.min(availSpace, 1000) - animationWidth) / 2,
+            transition: "left 300ms ease",
+          });
+        }
       }
     };
 
@@ -60,16 +105,18 @@ const NewSearch = () => {
   return (
     <main className="page-buffer page-container md:max-w-page">
       <PageTitle>New Search</PageTitle>
-      <div className="space-y-4 md:w-[450px] lg:w-[500px]">
+      <div
+        ref={paramsContainerRef}
+        className="space-y-4 md:w-[450px] lg:w-[500px]"
+      >
         <PlantLocationForm renderMode="card" hideFooter />
         <PlantNameForm renderMode="card" hideFooter />
       </div>
       <div
-        ref={animationContainerRef}
         style={animationContainerStyle}
         className="flex flex-col items-center justify-center max-md:w-full! small-screen:w-[450px] small-screen:my-8 big-screen:fixed big-screen:translate-y-1/2"
       >
-        <div className="max-w-fit space-y-6">
+        <div ref={animationRef} className="max-w-fit space-y-6">
           <PlantAnimation
             customNoDataMessage="All fields are optional"
             className="max-h-fit"
